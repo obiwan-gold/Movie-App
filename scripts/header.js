b1ecf7ab833cd2d059bc012b99a2c514b0d12940
Mr Gold <individualog@gmail.com>
2022-10-27 16:43:15 +0100

    Something was done

--- scripts/header.js
+++ scripts/header.js
@@ -18,18 +18,42 @@ function findMovies() {
 async function searchBar(searchInput) {
   // searchTerm is typed in parameters
   const URL = `https://omdbapi.com/?s=${searchInput}&page=1&apikey=fc1fef96`;
-  try {
-    const response = await fetch(`${URL}`);
-    const data = await response.json();
-    // Unsure what data.Response is doing here
-    console.log(data.Search)
-    if (data.Response === "True") {
-      displayMovie(data.Search);
-    }
-  } catch (error) {
-    console.log(error)
+  const response = await fetch(`${URL}`);
+  const data = await response.json();
+  // Unsure what data.Response is doing here
+  console.log(data.Search)
+  if (data.Response === "True") dropDown(data.Search);
+}
+
+
+//function which renders the searchlist:
+function dropDown(movies) {
+  //movies is an object of objects
+  for (let i = 0; i < movies.length; i++) {
+    /*movieRow, parent element, is a row of the searchlist, 
+    containing some details about each film*/
+    let movieRow = document.createElement('div')
+    movieRow.dataset.id = movies[i].imdbID
+    movieRow.classList.add('movie-row');
+
+
+    let movieThumbnail = document.createElement('img').classList.add('searchlist-movie-thumbnail');
+    // movieThumbnail.setAttribute('src', movie[i].Poster);
+    // movieThumbnail.setAttribute('alt', `Poster for ${movie[i].Title}`);
+
+    let movieName = document.createElement('h4').classList.add('searchlist-movie-titles');
+    let movieRating = document.createElement('p').classList.add('searchlist-movie-rating');
+    let movieYear = document.createElement('p').classList.add('searchlist-movie-year');
+
+    // movieName.textContent = movies[i].Title;
+    // movieRating.textContent = movies[i].Rating;
+    // movieYear.textContent = movies[i].Year;
+
+    // movieRow.append(movieThumbnail, movieName, movieRating, movieYear);
+    searchList.appendChild(movieRow)
   }
 }
+
 
 
 // Event listener for onclick & onkeyup
@@ -39,52 +63,3 @@ window.addEventListener('click', (event) => {
     searchList.classList.add('hide-search-list');
   }
 });
-
-
-/*
-// Function that takes in json data from line 11
-function displayMovie(movies) {
-
-  searchList.innerHTML = "";
-
-  for (let i = 0; i < movies.length; i++) {
-    // Creating a row that contains movie item title and image
-    let movieItem = document.createElement('div');
-
-    movieItem.dataset.id = movies[i].imdbID; // 
-    movieItem.classList.add('search-list-item');
-
-    searchList.appendChild(movieItem)
-  }
-//loadMovieDetails();
-}
-*/
-
-//function which renders the searchlist:
-function dropDown (movies) {
-  //movies is an object of objects
-    for (let i = 0; i < movies.length; i++) {
-      /*movieRow, parent element, is a row of the searchlist, 
-      containing some details about each film*/
-      let movieRow = document.createElement('div').classList.add('movie-row');
-      //children:
-      let movieThumbnail = document.createElement('img').classList.add('searchlist-movie-thumbnail');
-     // movieThumbnail.setAttribute('src', movie[i].Poster);
-     // movieThumbnail.setAttribute('alt', `Poster for ${movie[i].Title}`);
-
-      let movieName = document.createElement('h4').classList.add('searchlist-movie-titles');
-      let movieRating = document.createElement('p').classList.add('searchlist-movie-rating');
-      let movieYear = document.createElement('p').classList.add('searchlist-movie-year');
-
-      movieName.textContent = movies[i].Title;
-      movieRating.textContent = movies[i].Rating;
-      movieYear.textContent = movies[i].Year;
-      
-      movieRow.append(movieThumbnail, movieName, movieRating, movieYear);
-      searchList.appendChild(movieItem)
-    }
-    
-
-
-
-}
