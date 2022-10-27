f61e1fd46690397ebf6eceaaa8005b58766e7146
Mr Gold <individualog@gmail.com>
2022-10-27 12:52:33 +0100

    Added query selectors to build Search Bar functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,31 @@
 
 // Load movies from API
-async function searchBar(searchID) {
+async function searchBar(searchResult) {
   // searchID is typed in parameters
-  const URL = `http://www.omdbapi.com/?i=${searchID}&page=1&apikey=f0951f4b`;
+  const URL = `http://www.omdbapi.com/?s=${searchResult}&page=1&apikey=f0951f4b`;
   const res = await fetch(`${URL}`);
   const data = await res.json();
-  // console.log(data.Search);
-  if (data.Response == "True") displayMovie(data.Search);
+  console.log(data.Search);
+  // if (data.Response == "True") displayMovie(data.Search);
 }
 
-
-// If the user types in the search Bar
-window.onload = () => {
-  const searchField = document.getElementById('movie-searchBox');
-  searchField.onkeyup = (e) => {
-    searchBar(searchField.value)
+function findMovies() {
+  let searchResult = (movieSearchBox.value).trim();
+  if (searchTerm.length > 0) {
+    searchList.classList.remove('hide-search-list');
+    loadMovies(searchTerm);
+  } else {
+    searchList.classList.add('hide-search-list');
   }
 }
+
+// Event listener for onclick & onkeyup
+
+// window.addEventListener('click', (event) => {
+//   if (event.target.className != "search-bar-visibility") {
+//     searchList.classList.add('hide-search-list');
+//   }
+// });
+
 
 
