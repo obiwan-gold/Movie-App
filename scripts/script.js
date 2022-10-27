2c385a971c231a3b49f8164d260b8f8f3fb4b563
Mr Gold <individualog@gmail.com>
2022-10-27 12:34:00 +0100

    giasdfsdafasdfasdf

--- scripts/script.js
+++ scripts/script.js
@@ -1,57 +1,28 @@
 
-// 
-function searchBar(searchID) {
-  fetch(`http://www.omdbapi.com/?s=${searchID}&apikey=f0951f4b`)
-    .then(response => {
-      return response.json();
-    })
-
-    .then(data => {
-
-      console.log(data)
-    })
+// Load movies from API
+async function searchBar(searchID) {
+  // searchID is typed in parameters
+  const URL = `http://www.omdbapi.com/?i=${searchID}&page=1&apikey=f0951f4b`;
+  const res = await fetch(`${URL}`);
+  const data = await res.json();
+  // console.log(data.Search);
+  if (data.Response == "True") displayMovie(data.Search);
 }
 
-// 
 
-function createMovieCard(movieObject) {
+// If the user types in the search Bar
+window.onload = () => {
+  const searchField = document.getElementById('movie-searchBox');
+  searchField.onkeyup = (e) => {
+    searchBar(searchField.value)
+  }
+}
 
-  //parent Section:
-  const newMovieCard = document.createElement('Section').classList.add('movie-cards')
-  //child: movie title:
-  const movieTitle = document.createElement('h2').classList.add('movie-titles');
-  //child: card image:
-  const moviePoster = document.createElement('img').classList.add('posters');
-  moviePoster.setAttribute('src', `${movieObject.image}`);
-  moviePoster.setAttribute('alt', `movie poster for ${movieObject.title}`);
-  //child: section containing description, date and price:
-  const movieInfo = document.createElement('section').classList.add('movie-info');
-
-  //children of movieInfo section:
-  const movieReleaseYear = document.createElement('span').classList.add('release-years');
-  const movieRating = document.createElement('span').classList.add('ratings');
-  const movieRuntime = document.createElement('span').classList.add('runtimes');
-  const movieGenre = document.createElement('span').classList.add('genres');
-
-  //child: paragraph containing plot summary
-  const plotSummary = document.createElement('p').classList.add('summaries');
-
-  //adding content to all the created sections
-  movieTitle.innerText = movieObject.name;
-
-  movieReleaseYear.innerText = movieObject.Year;
-  movieRating.innerText = movieObject.Rating;
-  movieRuntime.innerText = movieObject.Runtime;
-  movieGenre.innerText = movieObject.Genre;
-
-  plotSummary.innerText = movieObject.plot
+// If the user clicks off the search bar, the search bar disappears.
+window.addEventListener('click', (event) => {
+  if (event.target.className != "search-bar-visibility") {
+    searchList.classList.add('hide-search-list');
+  }
+});
 
 
-  movieInfo.append(movieReleaseYear, movieRating, movieRuntime, movieGenre);
-  newMovieCard.append(movieTitle, moviePoster, movieInfo, plotSummary);
-
-  const movieCardsSection = document.getElementById('movie-cards-section');
-  movieCardsSection.appendChild(newMovieCard);
-
-
-};
